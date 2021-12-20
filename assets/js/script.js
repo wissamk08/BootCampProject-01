1de5f8956b15d551e74e7334580797154a8c91dc
Ian <new@news-MacBook-Pro.local>
2021-12-20 13:24:00 -0700

    addition var to JS

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var ApiKey = "5kUQVR6ehDpKIKtoUyoViEDjNNLj9MHv"
+
+var company = $(#search-input).val()
+
+companySearchURL = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17?apiKey=5kUQVR6ehDpKIKtoUyoViEDjNNLj9MHv"
