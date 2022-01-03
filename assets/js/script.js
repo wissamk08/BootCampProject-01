0cd0727e94bd606abcaca2a390d24a87fc5db409
wissam <wissamk08@gmail.com>
2022-01-02 17:16:11 -0700

    support page

--- assets/js/script.js
+++ assets/js/script.js
@@ -297,3 +297,12 @@ function darkMode() {
     darkElement.classList.toggle("dark-mode");
 }
 
+// var submitBtn = document.getElementById("subb")
+
+// function subBtn(){
+//     document.getElementById("confirm").textContent =
+//    "Your support ticket has been submitted we will get back to you as soon as possible";
+//    console.log("anything")
+// }
+
+// submitBtn.addEventListener("submit", subBtn)
